27ca87f2cd051db55f6486703a8b8b0b815239f6
Adrien Rougny <adrienrougny@gmail.com>
2024-04-30 13:14:55 +0200

    added test.py

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,25 @@
 import neomodel
 
-import momapy
+import momapy.core
 import momapy.sbgn.pd
 import momapy.sbgn.io.sbgnml
 import momapy.io
 
-import momapy_kg.builder
-import momapy_kg.sbgn
+import momapy_kg.neo4j
+import momapy_kg.utils
 import credentials
 
 if __name__ == "__main__":
-    neomodel.config.DATABASE_URL = f"bolt://{credentials.USER_NAME}:{credentials.PASSWORD}@{credentials.HOST_NAME}:7687"
-    results, meta = neomodel.db.cypher_query("MATCH (n) DETACH DELETE n")
-    # momapy_neo4j.utils.pretty_print(
-    #     momapy_neo4j.builder.get_or_make_node_cls(
-    #         momapy.sbgn.pd.Macromolecule
-    #     ),
-    #     max_depth=3,
-    # )
-    m = momapy.io.read("Neuroinflammation.sbgn")
-    momapy_kg.builder.save_node_from_object(m)
+    # neomodel.config.DATABASE_URL = f"bolt://{credentials.USER_NAME}:{credentials.PASSWORD}@{credentials.HOST_NAME}:7687"
+    # results, meta = neomodel.db.cypher_query("MATCH (n) DETACH DELETE n")
+    # m = momapy.io.read("Neuroinflammation.sbgn")
+    # # momapy_kg.neo4j.save_node_from_object(m)
+    model_node_cls = momapy_kg.neo4j.get_or_make_node_cls(
+        momapy.sbgn.pd.SBGNPDModel
+    )
+    momapy_kg.neo4j.make_doc(
+        momapy.sbgn.pd,
+        mode="neomodel",
+        output_file_path="test.html",
+        exclude=[momapy.core.LayoutElement, momapy.builder.Builder],
+    )
