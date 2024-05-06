8df78f8720bfaa68543d4c2fc39baf7b81148fbc
Adrien Rougny <adrienrougny@gmail.com>
2024-05-06 10:09:03 +0200

    udpate

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
-import neomodel
+import os
+import dataclasses
 
 import momapy.core
 import momapy.sbgn.pd
@@ -9,17 +10,38 @@ import momapy_kg.neo4j
 import momapy_kg.utils
 import credentials
 
+
+@dataclasses.dataclass
+class Collection:
+    name: str
+    maps: list[momapy.core.Map] = dataclasses.field(default_factory=list)
+
+
+def list_dir(path):
+    files = []
+    for file_name in os.listdir(path):
+        if not file_name.startswith("."):
+            files.append((file_name, os.path.join(path, file_name)))
+    return files
+
+
 if __name__ == "__main__":
-    # neomodel.config.DATABASE_URL = f"bolt://{credentials.USER_NAME}:{credentials.PASSWORD}@{credentials.HOST_NAME}:7687"
-    # results, meta = neomodel.db.cypher_query("MATCH (n) DETACH DELETE n")
-    # m = momapy.io.read("Neuroinflammation.sbgn")
-    # # momapy_kg.neo4j.save_node_from_object(m)
-    model_node_cls = momapy_kg.neo4j.get_or_make_node_cls(
-        momapy.sbgn.pd.SBGNPDModel
+    momapy_kg.neo4j.connect(
+        credentials.HOST_NAME, credentials.USER_NAME, credentials.PASSWORD
     )
-    momapy_kg.neo4j.make_doc(
-        momapy.sbgn.pd,
-        mode="neomodel",
-        output_file_path="test.html",
-        exclude=[momapy.core.LayoutElement, momapy.builder.Builder],
-    )
+    momapy_kg.neo4j.delete_all()
+    c = Collection("PD")
+    for _, file_path in list_dir(
+        "/home/rougny/research/commute/commute_dm_develop/build/maps/pd/sbgn/"
+    ):
+        print(file_path)
+        m = momapy.io.read(file_path)
+        c.maps.append(m)
+        break
+    momapy_kg.neo4j.save_node_from_object(c)
+    # momapy_kg.neo4j.make_doc(
+    #     momapy.sbgn.pd,
+    #     mode="neomodel",
+    #     output_file_path="test.html",
+    #     exclude=[momapy.core.LayoutElement, momapy.builder.Builder],
+    # )
