89319792bf369682e99f8ad63ea24439e7ef67c9
Adrien Rougny <adrienrougny@gmail.com>
2024-07-03 13:53:35 +0200

    update

--- tests/test_celldesigner.py
+++ tests/test_celldesigner.py
@@ -11,5 +11,7 @@ if __name__ == "__main__":
         credentials.HOST_NAME, credentials.USER_NAME, credentials.PASSWORD
     )
     momapy_kb.neo4j.core.delete_all()
-    m = momapy.io.read("/home/rougny/poubelle/test.xml")
+    m = momapy.io.read(
+        "/home/rougny/presentations/disease_maps_meeting_jun_2024/src/images/celldesigner_example.xml"
+    )
     momapy_kb.neo4j.core.save_node_from_object(m, object_to_node_mode="hash")
