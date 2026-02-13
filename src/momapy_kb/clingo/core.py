02d996d2c00b3f7a2e808b45e6d89bd6ef42b303
Adrien Rougny <adrienrougny@gmail.com>
2026-02-13 16:18:21 +0100

    feat: added clingo from fieldz_kb.clingo

---
+++ src/momapy_kb/clingo/core.py
@@ -0,0 +1,4 @@
+import fieldz_kb.clingo.core
+
+make_facts_from_object = fieldz_kb.clingo.core.make_facts_from_object
+make_ontology_rules_from_type = fieldz_kb.clingo.core.make_ontology_rules_from_type
