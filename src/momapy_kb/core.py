d818695f66cd732a0a057f40aee4c13117e4a2de
Adrien Rougny <adrienrougny@gmail.com>
2026-04-07 14:28:20 +0200

    refactor: restore generic frozendict type hints

--- src/momapy_kb/core.py
+++ src/momapy_kb/core.py
@@ -5,16 +5,27 @@ import frozendict
 import momapy.core.model
 import momapy.core.map
 import momapy.core.layout
+import momapy.core.elements
+import momapy.sbml.core
 
 
 @dataclasses.dataclass(frozen=True)
 class CollectionEntry:
     id_: str
     model: momapy.core.map.Map | momapy.core.model.Model | momapy.core.layout.Layout
-    rdf_annotations: frozendict.frozendict | None = None
+    rdf_annotations: (
+        frozendict.frozendict[
+            momapy.core.elements.MapElement, frozenset[momapy.sbml.core.RDFAnnotation]
+        ]
+        | None
+    ) = None
     file_path: str | None = None
-    ids: frozendict.frozendict | None = None
-    notes: frozendict.frozendict | None = None
+    ids: (
+        frozendict.frozendict[momapy.core.elements.MapElement, frozenset[str]] | None
+    ) = None
+    notes: (
+        frozendict.frozendict[momapy.core.elements.MapElement, frozenset[str]] | None
+    ) = None
 
 
 @dataclasses.dataclass(frozen=True)
